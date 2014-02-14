32f299c66bec739b1cb0b4f4a35b1b9b61467b88
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-02-14 18:48:48 -0500

    Update rspec tests for teamspeak3

--- spec/classes/teamspeak3_spec.rb
+++ spec/classes/teamspeak3_spec.rb
@@ -2,10 +2,26 @@ require 'spec_helper'
 
 describe 'teamspeak3' do
   context 'supported architecture' do
-    describe "teamspeak3 class without any parameters on amd64" do
+    describe 'staging class without any parameters' do
       let(:params) {{ }}
       let(:facts) {{
         :architecture => 'amd64',
+        :kernel       => 'Linux',
+        :osfamily     => 'Debian',
+      }}
+
+      it { should contain_class('staging::params') }
+      it { should contain_class('staging') }
+
+      it { should contain_file('/opt/staging') }
+    end
+
+    describe 'teamspeak3 class without any parameters on amd64' do
+      let(:params) {{ }}
+      let(:facts) {{
+        :architecture => 'amd64',
+        :kernel       => 'Linux',
+        :osfamily     => 'Debian',
       }}
 
       it { should compile.with_all_deps }
@@ -16,16 +32,25 @@ describe 'teamspeak3' do
       it { should contain_class('teamspeak3::service').that_subscribes_to('teamspeak3::config') }
       it { should contain_class('teamspeak3') }
 
-      it { should contain_file("/opt/teamspeak3-server_linux-#{:architecture}") }
+      it { should contain_file('/opt/staging/teamspeak3') }
+      it { should contain_staging__deploy("teamspeak3-server_linux-#{facts[:architecture]}-3.0.10.3.tar.gz") }
+      it { should contain_staging__file("teamspeak3-server_linux-#{facts[:architecture]}-3.0.10.3.tar.gz") }
+      it { should contain_staging__extract("teamspeak3-server_linux-#{facts[:architecture]}-3.0.10.3.tar.gz") }
+      it { should contain_exec("/opt/staging/teamspeak3/teamspeak3-server_linux-#{facts[:architecture]}-3.0.10.3.tar.gz") }
+      it { should contain_exec("extract teamspeak3-server_linux-#{facts[:architecture]}-3.0.10.3.tar.gz") }
+
+      it { should contain_file('/etc/default/teamspeak3') }
+      it { should contain_file('/etc/init.d/teamspeak3') }
 
       it { should contain_service('teamspeak3') }
-      it { should contain_package('teamspeak3').with_ensure('present') }
     end
 
-    describe "teamspeak3 class without any parameters on i386" do
+    describe 'teamspeak3 class without any parameters on i386' do
       let(:params) {{ }}
       let(:facts) {{
         :architecture => 'i386',
+        :kernel       => 'Linux',
+        :osfamily     => 'Debian',
       }}
 
       it { should compile.with_all_deps }
@@ -37,21 +62,29 @@ describe 'teamspeak3' do
       it { should contain_class('teamspeak3::service').that_subscribes_to('teamspeak3::config') }
       it { should contain_class('teamspeak3') }
 
-      it { should contain_file('/opt/teamspeak3-server_linux-x86') }
+      it { should contain_file('/opt/staging/teamspeak3') }
+      it { should contain_staging__deploy('teamspeak3-server_linux-x86-3.0.10.3.tar.gz') }
+      it { should contain_staging__file('teamspeak3-server_linux-x86-3.0.10.3.tar.gz') }
+      it { should contain_staging__extract('teamspeak3-server_linux-x86-3.0.10.3.tar.gz') }
+      it { should contain_exec('/opt/staging/teamspeak3/teamspeak3-server_linux-x86-3.0.10.3.tar.gz') }
+      it { should contain_exec('extract teamspeak3-server_linux-x86-3.0.10.3.tar.gz') }
+
+      it { should contain_file('/etc/default/teamspeak3') }
+      it { should contain_file('/etc/init.d/teamspeak3') }
 
       it { should contain_service('teamspeak3') }
-      it { should contain_package('teamspeak3').with_ensure('present') }
     end
   end
 
   context 'unsupported architecture' do
     describe 'teamspeak3 class without any parameters on Solaris/Nexenta' do
       let(:facts) {{
-        :osfamily        => 'Solaris',
-        :operatingsystem => 'Nexenta',
+        :architecture => 'sparc',
+        :kernel       => 'sunos',
+        :osfamily     => 'Solaris',
       }}
 
-      it { expect { should contain_package('teamspeak3') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
+      it { expect { should contain_package('teamspeak3') }.to raise_error(Puppet::Error, /sunos not supported/) }
     end
   end
 end
