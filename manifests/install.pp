1e6db29efae0b9c8a00878dc38a9b5f19cde3489
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-02-13 23:57:34 -0500

    Add staging::deploy of the teamspeak release

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,16 @@
 class teamspeak3::install {
   include teamspeak3::params
 
-  package { $teamspeak3::params::package_name:
-    ensure => present,
+  if ! defined( staging ) {
+    class { 'staging': }
+  }
+
+  $release_dir  = "teamspeak3-server_linux-${teamspeak3::params::ts3_arch}"
+  $release_file = "${release_dir}-${teamspeak3::version}.tar.gz"
+
+  staging::deploy { $release_file:
+    source  => "http://files.teamspeak-services.com/releases/${teamspeak3::version}/${release_file}",
+    target  => $teamspeak3::basedir,
+    creates => "${teamspeak3::basedir}/${release_dir}",
   }
 }
